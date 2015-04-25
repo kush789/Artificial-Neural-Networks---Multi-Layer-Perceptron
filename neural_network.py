e027b206c218f855b11a50b0dfc1632ad303fc1b
kush789 <singh.kushagra.1996@gmail.com>
2015-04-25 18:33:58 +0530

    Changed node architecture, fixed feed forward.

--- neural_network.py
+++ neural_network.py
@@ -2,7 +2,7 @@
 #																	#
 # 	 This is free software; you can redistribute it and/or modify   #
 #    it under the terms of the GNU General Public License as 		#
-#    published by the Free Software Foundation; either version 2 	#
+#    published by the Free Software Foundation; either version 2  	#
 #    of the License, or (at your option) any later version. 		#
 #																	#
 #    This code is distributed in the hope that it will be useful,	#
@@ -16,9 +16,11 @@
 #																	#
 #####################################################################
 
+########################### Dependencies ############################
+
 import numpy
 
-####################### Activation functions #########################
+####################### Activation functions ########################
 
 def sigmoid(x):
 	return 1.0/(1.0 + numpy.exp(-x))
@@ -26,19 +28,21 @@ def sigmoid(x):
 def sigmoid_derivative(x):
 	return sigmoid(x) * (1.0 - sigmoid(x))
 
-########################### Node class ###############################
+########################### Node class ##############################
 
-""" 1. Input value, list of weight of edge to each node in hidden layers 
-	2. Weight[0][0] means weight from first node in current layer to 
+""" 1. input_value -> Input data 
+	2. output_value -> sigmoid(input_value)
+	3. Weight[0][0] means weight from first node in current layer to 
 	first node in next layer """
 
 class node:
 
-	def __init__(self, value, weight):
-		self.value = value
+	def __init__(self, input_value, weight, output_value = 0):
+		self.input_value = input_value
+		self.output_value = output_value
 		self.weight = weight
 
-####################### Input layer class #############################
+####################### Input layer class ###########################
 
 """ List of nodes, no bias list (input has no bias input) """
 
@@ -46,14 +50,16 @@ class input_layer:
 
 	def __init__(self, input_nodes, values, weights):
 		self.nodes = []
-		self.values = []
 		self.total = input_nodes
 
 		for i in range(input_nodes):
-			self.nodes.append(node(values[i], weights[i]))
-			self.values.append(0)
+			self.nodes.append(node(0, weights[i], values[i]))
 
-################## Hidden and Output layer class ######################
+	def set_output_values(self, output_values):
+		for i in range(self.total):
+			self.nodes[i].output_value = output_values[i]
+
+################## Hidden and Output layer class ####################
 
 """ List of nodes, and a bias list.
 	Bias[i] means bias of input to node i of current layer """
@@ -63,15 +69,15 @@ class layer:
 	def __init__(self, num_nodes, weights, bias):
 		self.nodes = []
 		self.bias = []
-		self.values = []
+		self.delta = []
 		self.total = num_nodes
 
 		for i in range(num_nodes):
 			self.nodes.append(node(0, weights[i]))
 			self.bias.append(bias[i])
-			self.values.append(0)
+			self.delta.append(0)
 
-################# Artificial Neural Network class #####################
+################# Artificial Neural Network class ###################
 
 """ As of now, only one hidden layer """
 
@@ -102,39 +108,46 @@ class neural_network:
 
 	def set_hidden_layer_values(self):
 
-		""" Sets the values of the hidden layer """
+		""" Sets the values of the hidden layer nodes """
 
 		for i in range(self.hidden_layer.total):
 			temp_sum = 0.0
 
 			for j in range(self.input_layer.total):
-				temp_sum += (self.input_layer.nodes[j].value * 
+				temp_sum += (self.input_layer.nodes[j].output_value * 
 							self.input_layer.nodes[j].weight[i])
 
 			temp_sum += self.hidden_layer.bias[i]
-			self.hidden_layer.nodes[i].value = sigmoid(temp_sum)
+			self.hidden_layer.nodes[i].input_value = temp_sum
+			self.hidden_layer.nodes[i].output_value = sigmoid(temp_sum)
 
 	def set_output_layer_values(self):
 
-		""" Sets the values of the output layer """
+		""" Sets the values of the output layer nodes """
 
 		for i in range(self.output_layer.total):
 			temp_sum = 0.0
 
 			for j in range(self.hidden_layer.total):
-				temp_sum += (self.hidden_layer.nodes[j].value * 
+				temp_sum += (self.hidden_layer.nodes[j].output_value * 
 							 self.hidden_layer.nodes[j].weight[i])
 
 			temp_sum += self.output_layer.bias[i]
-			self.output_layer.nodes[i].value = sigmoid(temp_sum)
+			self.output_layer.nodes[i].input_value = temp_sum
+			self.output_layer.nodes[i].output_value = sigmoid(temp_sum)
 
-	def forward_propogate(self, input_data):
+	def feed_forward(self, input_data):
 
 		""" Sets hidden layer values and output 
 			layer values based upon input values """
 
-		for i in range(self.input_layer.total):
-			self.input_layer.nodes[i].value = input_data[i]
-
+		self.input_layer.set_output_values(input_data)
 		self.set_hidden_layer_values()
 		self.set_output_layer_values()
+
+	""" Learning through Back Propogation """
+
+	def delta_calculation(self, output_data):
+
+		""" Calculates the delta input_value for each 
+			node in the network """
